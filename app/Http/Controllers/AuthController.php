93a35e754e1911727ae6090010dd7f20d0dd23c6
ZazabT <157392321+ZazabT@users.noreply.github.com>
2024-09-21 21:52:53 +0300

    added register api

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //sign up or register
+    
+    public function register(Request $request){
+        $validatedUser = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'string',
+                'email', // Ensure it's a valid email format
+                'max:255', // Limit length
+                'unique:users,email', // Ensure it's unique in the users table
+                'regex:/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/', // Custom regex for more stringent email validation
+            ],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $user = User::create($validatedUser);
+        return $user;
+    }
+
+
+
+    // login
+
+    public function login(Request $request){    
+
+    }
+
+    // logout 
+
+    public function logout(Request $request){
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,18 @@
 use App\Http\Controllers\ContactController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-
+// contact crud routes
 Route::apiResource('contacts' , ContactController::class );
+
+
+
+// user auth routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
